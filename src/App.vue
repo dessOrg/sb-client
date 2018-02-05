d83ff8f85407b938c38e07c8a6ffdd4f701dc774
dessHub <deskip95@gmail.com>
2018-02-05 10:08:45 +0300

    home components

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+     <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+       <span class="icon-bar"></span>
+       <span class="icon-bar"></span>
+       <span class="icon-bar"></span>
+      </button>
+      <router-link to="/" class="nav navbar-brand"><img src="./assets/sblogo.png"></router-link>
+     </div>
+     <div class="collapse navbar-collapse">
+      <ul class="navbar-nav nav pull-right">
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/">Team</router-link></li>
+        <li><router-link to="/">Services</router-link></li>
+        <li><router-link to="/">Startups</router-link></li>
+        <li><router-link to="/">Events</router-link></li>
+        <li><router-link to="/">About</router-link></li>
+      </ul>
+     </div>
+    </div>
+  </nav>
+  <div id="app">
+    
+    <router-view/>
+  </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+}
+.navbar-default {
+  background : transparent;
+  border : none;
+  color : white;
+}
+.navbar {
+  background : transparent;
+}
+.navbar-default .navbar-nav > li > a {
+   color : #fff;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,244 @@
+
+<template>
+<div class="">
+ <div class="slider">
+
+  <div class="slider-text">
+   <h3>THE TECHNOLOGY OPEN SPACE</h3>
+   <h4>MOMBASA </h4>
+   <span>Technology <strong>|</strong></span>
+   <span>Innovation</span>
+  </div>
+
+ </div>
+ <div class="about">
+  <div class="heading">
+   <h2>Who We Are</h2>
+  </div>
+
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-12">
+    <div class="title">
+     <h3><strong><b> SwahiliBox is a project of M-Power (CBO). The Technology Open Space in Mombasa, Kenya </b></strong></h3>
+    </div>
+     <p>Swahilibox focuses on socio-economic technology empowerment, inspiring and developing individuals and help them develop new and innovative ideas through networking, access to training and support and professional mentoring and coaching. It brings together investors, donors, entrepreneurs, experts, artists, and other individuals and provides them with an opportunity to share knowledge, learn, find mentors and expound on their talents, abilities and innovative ideas that will lead to the development of Kenya and globally at large.</p>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-12">
+     <img src="../assets/about.jpg" class="img-responsive" style="width:100%; height:250px;">
+    </div>
+  </div>
+  </div>
+ </div>
+
+ <div class="strip">
+   <h2>Swahilibox provides a premier co-working space, social innovation centre and offers incubation support programme for new startup ideas leveraging the Swahilibox's resources and networks.</h2>
+ </div>
+
+ <div class="services">
+  <div class="container-fluid">
+   <div class="row">
+     <div class="col-md-4 col-xs-12">
+       <div class="icon">
+         <span><i class="fa fa-cogs fa-3x" style="color:#FFD600;"></i></span>
+       </div>
+       <div class="service-cont">
+         <h4>TECHNOLOGY</h4>
+         <p>Technologies to power socio and economy innovation.</p>
+       </div>
+     </div>
+     <div class="col-md-4 col-xs-12">
+       <div class="icon">
+         <span><i class="fab fa-accusoft fa-3x"></i></span>
+       </div>
+       <div class="service-cont">
+         <h4>INNOVATION</h4>
+         <p></p>
+       </div>
+     </div>
+     <div class="col-md-4 col-xs-12">
+       <div class="icon">
+         <span><i class="fab fa-react fa-3x" style="color: #FFC107;"></i></span>
+       </div>
+       <div class="service-cont">
+         <h4>IOT</h4>
+         <p></p>
+       </div>
+     </div>
+   </div>
+  </div>
+ </div>
+
+ <div class="member" style="padding-top: 10px;">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8" style="text-align: left;">
+        <h4 style="color:#FF9800;">BECOME A MEMBER</h4>
+        <h2>Join now to become part of this highly 
+        dynamic, interactive environment.</h2>
+      </div>
+      <div class="col-md-4" style="padding-top: 40px;">
+         <img src="../assets/sblogo.png">
+      </div>
+    </div>
+  </div>
+ </div>
+
+<div class="partners">
+<div class="container-fluid">
+<div class="row">
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic1.jpg" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic2.jpg" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic3.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic4.jpg" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic5.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic6.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic7.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic8.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic16.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic10.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic11.jpg" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic12.jpg" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic13.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic14.jpg" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic15.jpg" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic9.jpg" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic17.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic18.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic19.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic20.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic21.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic22.png" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic15.jpg" class="img-responsive img-part">
+ </div>
+ <div class="col-lg-2 col-md-2 col-xs-4">
+   <img src="../assets/partners/pic4.jpg" class="img-responsive img-part">
+ </div>
+</div>
+</div>
+</div>
+
+<div class="">
+
+</div>
+
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+.slider {
+  background-color: green;
+  background-image : url('../assets/b.jpg');
+  background-size : cover;
+  background-repeat : no-repeat;
+  margin-top : -40px;
+  height : 80vh;
+}
+.slider-text {
+  padding-top : 210px;
+  color: #FF9800;
+}
+.slider-text span {
+  color: white;
+}
+.slider-text span strong {
+  color: #FF9800;
+}
+.about {
+  text-align : left;
+  padding : 10px;
+}
+.about .heading {
+  color : #FF9800;
+}
+.strip {
+  color : #a3a3a3;
+  background-color : #000;
+  text-align : centre;
+  padding : 10px;
+  margin-top : 10px;
+}
+.services {
+  padding-top : 30px;
+  text-align : center;
+  background-color : #f4f4f5;
+}
+.icon span {
+  border-radius : 50%;
+  background-color : #1255a6;
+  padding : 5px;
+}
+.icon span i {
+  color: green;
+  background-color : red;
+}
+.service-cont {
+  padding-left : 60px;
+  padding-right : 60px;
+}
+.partners {
+  margin-top : 40px;
+  background-color : #C5CAE9;
+  padding :10px;
+}
+.img-part {
+  height : 70px;
+  width : 100px;
+}
+</style>
